71a5975ffe18987d3886a3f0aadf3c43942bc066
Ashwin Chandran <ashwinchandran13@gmail.com>
2020-12-17 20:19:34 +0530

    Completed middleware and router prior to test

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -88,15 +88,8 @@ func GetJob(w http.ResponseWriter, r *http.Request) {
 	//get the jobid from request params, key is "id"
 	params := mux.Vars(r)
 
-	//convert id type string to int
-	id, err := strconv.Atoi(params["id"])
-
-	if err != nil {
-		log.Fatalf("Unable to convert string into int.  %v", err)
-	}
-
 	//calling getJob function with job id to retrieve that job details
-	job, err := getJob(int64(id))
+	job, err := getJob(string(params["name"]))
 
 	if err != nil {
 		log.Fatalf("Unable to get user. %v", err)
@@ -114,14 +107,8 @@ func FilterJob(w http.ResponseWriter, r *http.Request) {
 	//get the job name from request params
 	params := mux.Vars(r)
 
-	name, err := params["name"]
-
-	if err != nil {
-		log.Fatalf("No job name recieved. $v", err)
-	}
-
 	//call the filterJob function with job name to retrieve a list of posted job with that name
-	job, err := filterJob(string(name))
+	job, err := filterJob(string(params["name"]))
 
 	if err != nil {
 		log.Fatalf("Unable to get any valid job. %v", err)
@@ -131,6 +118,7 @@ func FilterJob(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(job)
 }
 
+// DeleteJob to delete entries
 func DeleteJob(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
@@ -138,4 +126,165 @@ func DeleteJob(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+	//get job id from the request params
+	params := mux.Vars(r)
+
+	//convert the id in string to int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to covert string id into int. %v", err)
+	}
+
+	//call the deleteJob, convert int to int64
+	deletedRows := deleteJob(int64(id))
+
+	//format the message string
+	msg := fmt.Sprintf("job deleted successfully. Rows affected %v", deletedRows)
+
+	// format response message
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+//------------Handler Functions check 2------------
+// insert user in DB
+
+func insertJob(job models.Job) int64 {
+
+	// create postgres DB connection
+	db := createConnection()
+
+	// close db connection
+	defer db.Close()
+
+	// create the insert sql queery
+	// returning jobid will return the id of the inserted job
+	sqlStatement := `INSERT INTO jobs (jobname, location, openings) VALUES ($1, $2, $3) RETURNING jobid`
+
+	// inserted id will store in this id
+	var id int64
+
+	// execute the sql statement
+	// scan function will save insert id in the id
+	err := db.QueryRow(sqlStatement, job.Name, job.Location, job.Openings).Scan(&id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+
+	fmt.Printf("Inserted a single recored %v", id)
+
+	// return the inserted id
+	return id
+}
+
+func getJob(name string) (models.Job, error) {
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create a job of models.Job type
+	var job models.Job
+
+	// create the select sql query
+	sqlStatement := `SELECT * FROM jobs WHERE jobname=$1`
+
+	// execute the sql statement
+	row := db.QueryRow(sqlStatement, name)
+
+	// unmarshal the row object to job
+	err := row.Scan(&job.ID, &job.Name, &job.Openings, &job.Location)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Printf("No entry with job name as: %v", name)
+		return job, nil
+	case nil:
+		return job, nil
+	default:
+		log.Fatalf("Unable to scan the row. %v", err)
+	}
+
+	// return empty job on error
+	return job, err
+}
+
+func filterJob(name string) ([]models.Job, error) {
+	//create the progress db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	var jobs []models.Job
+
+	// create select sql query
+	sqlStatement := `SELECT * FROM jobs WHERE jobname=$1 AND location=$2`
+
+	// execute the sql statement
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		log.Fatalf("Unable to execute query. %v", err)
+	}
+
+	// close the statement
+	defer rows.Close()
+
+	// iterating over rows
+	for rows.Next() {
+		var job models.Job
+
+		// unmarshall the row object to user
+		err = rows.Scan(&job.ID, &job.Name, &job.Openings, &job.Location)
+
+		if err != nil {
+			log.Fatalf("Unable to scan the row. %v", err)
+		}
+
+		jobs = append(jobs, job)
+
+	}
+
+	// return empty job on error
+	return jobs, err
+}
+
+// delete user in the DB
+func deleteJob(id int64) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create the delete sql query
+	sqlStatement := `DELETE FROM jobs WHERE jobid=$1`
+
+	// execute the sql statement
+	res, err := db.Exec(sqlStatement, id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute query. %v", err)
+	}
+
+	// check how many rows affected
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total entries deleted :%v", rowsAffected)
+
+	return rowsAffected
 }
--- router/router.go
+++ router/router.go
@@ -7,14 +7,14 @@ import (
 )
 
 //Router is exported and used in main.go
-func Router *mux.Router() {
+func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/job/{id}", middleware.GetJob).Methods("GET", "OPTIONS") 
-	router.HandleFunc("/api/job/{name}", middleware.FilterJob).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/searchjob/{name}", middleware.GetJob).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/filterjob/{name}", middleware.FilterJob).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/newjob", middleware.PostJob).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/deletejob/{id}", middleware.DeleteJob).Methids("DELETE", "OPTIONS")
-	
+	router.HandleFunc("/api/deletejob/{id}", middleware.DeleteJob).Methods("DELETE", "OPTIONS")
+
 	return router
-} 
+}
